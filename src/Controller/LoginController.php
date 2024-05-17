c18029c7037c6c4642c04973b210635b0a176123
EauTarie <loubeau.alexandre3@gmail.com>
2024-05-17 16:57:31 +0200

    return user to home page if already logged

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,13 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, UserInterface $user): Response
     {
+        $userId = $user->getUserIdentifier();
+        if ($userId) {
+            return $this->render('home/index.html.twig', [
+                'controller_name' => 'HomeController'
+            ]);
+        }
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
